6a1ee7422d1b94bc253208051f174da2bfd67e74
Arvydas Pakulis <arvydas.pakulis@derivco.ee>
2017-04-13 14:28:44 +0300

    initial file upload functionality

--- index.js
+++ index.js
@@ -6,60 +6,82 @@ var fs = require('fs');
 var path = require('path');
 var _ = require('underscore');
 var express = require('express');
+//var router = express.Router();
+//var bodyParser = require('body-parser');
+var busboy = require('connect-busboy'); //middleware for form/file upload
+var fs = require('fs-extra');       //File System - for file manipulation
 var opn = require('opn')
+var formidable = require('formidable');
+var xml2js = require('xml2js');
+var parser = new xml2js.Parser();
 
-var postfixConnector = "-";
-var postfix = process.argv[2];
+var modif = process.argv[2];
 
 
 			//printint informative head;
-			process.stdout.write("++++++++++++++++++++++++++++++++++++++++++++++++++");
-			//for(var i=0; i<=process.argv[2].toString().length; i++){
-				//process.stdout.write("+");
-			//}
+			process.stdout.write("+++++++++++++++++++++++++++++");
 			process.stdout.write("\n");
-			console.log("fonts xml bulk modify");
-			process.stdout.write("++++++++++++++++++++++++++++++++++++++++++++++++++");
-			//for(var i=0; i<=process.argv[2].toString().length; i++){
-				//process.stdout.write("+");
-			//}
-			process.stdout.write("\n");
+			console.log("font xml modificator");
 			//eof of informative head;
 
 			
 //server initialization;
 var app = express();
+app.use(busboy());
+app.post('/', function(req, res){
+	var tempFileName = "";
+	var fullFilePath = "";
+	var form = new formidable.IncomingForm();
+	//specify that we want to allow the user to upload multiple files in a single request
+	form.multiples = true;
+	form.uploadDir = path.join(__dirname, '/uploads');
+	form.on('file', function(field, file) {
+		tempFileName = new Date().toDateString().split(" ").join("")
+			+"_"
+			+new Date().toGMTString().split(" ")[4].split(":").join("")
+			+"_"
+			+file.name;
+		fullFilePath = path.join(form.uploadDir, tempFileName);
+		fs.rename(file.path, fullFilePath);
+	});
+  form.on('error', function(err){console.log('An error has occured: \n' + err);});
+  // once all the files have been uploaded, send a response to the client
+  form.on('end', function() {
+	//res.write();
+    res.end('success');
+	readUploadedFile(fullFilePath);
+  });
+  form.parse(req);
+});
+
+
+
+
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+	res.writeHead('content-type','text/html');
+	res.write('<html>');
+	res.write('Hello World!<br>');
+	res.write('<form id="fileSelectionForm" method="post" action="" enctype="multipart/form-data">');
+	res.write('<input id="upload-input" type="file" name="uploads[]" multiple="multiple"><br>');
+	res.write('</form>');
+	res.write('<script src="fileInput.js"></script>');
+	res.write('</html>');
+	res.end();
+});
+
 
 app.listen(5001, function () {
-  console.log('Example app listening on port 5001!')
+	process.stdout.write('...is listening on port 5001!');
+	process.stdout.write("\n");
+	process.stdout.write("+++++++++++++++++++++++++++++");
 })
 
 opn('http://localhost:5001', {app: 'chrome'});
 //eof server initialization;
 
 
-const folder = '.';
-var fileNames = [];
-var currentItemWaittingForStat = -1;
-
-
-fs.readdir(folder, (err, files) => {
-	files.forEach(file => {
-		//console.log(file);
-		fileNames.push(file.toString());
-	});
-	interpreterList();
-})
-
-function interpreterList(){
-	//console.log(fileNames);
-	currentItemWaittingForStat = -1;
-	//getNextItemInfo();
-}
 
 function getNextItemInfo(){
 	currentItemWaittingForStat++;
@@ -69,39 +91,14 @@ function getNextItemInfo(){
 	}
 }
 
-function interpreterItem(err, res){
-	if(res){
-		console.log("progress: " + colors.bgWhite.black((currentItemWaittingForStat+1) + "of" + fileNames.length));
-		if(res.isFile()){
-			console.log("file: "+String(fileNames[currentItemWaittingForStat]));
-			if(fileNames[currentItemWaittingForStat].indexOf(postfixConnector+""+postfix) > -1){
-				var targetDuplicate =
-					fileNames[currentItemWaittingForStat].substr(0,fileNames[currentItemWaittingForStat].indexOf(postfixConnector+""+postfix))
-					+
-					path.extname(fileNames[currentItemWaittingForStat]);
-				console.log("files has duplicate without postfix: " + findFile(targetDuplicate));
-				fs.unlink(targetDuplicate, (err, res2) => {
-					if(err) console.log(err.message);
-					console.log("file deleted: "+targetDuplicate);
-					console.log("renaming file with postfix: "+fileNames[currentItemWaittingForStat]);
-					fs.rename(fileNames[currentItemWaittingForStat], targetDuplicate, (err, res3) => {if(err)throw err;console.log("successfully renamed");});
-					getNextItemInfo();
-				});
-			}else{
-				getNextItemInfo();
-			}
-		}else{
-			console.log("probably directory");
-			getNextItemInfo();
-		}
-	}else{
-		console.log("unrecognized or already deleted");
-		getNextItemInfo();
-	}
-}
-
-function findFile(id){
-	return _.some(fileNames, function(c) {
-		return c == id; 
+function readUploadedFile(target){
+	console.log(">readUploadedFile");
+	//var content = fs.readFileSync(target, 'utf8');
+	//console.log(content);   // Put all of the code here (not the best solution)
+	fs.readFile(target, function(err, data) {
+		parser.parseString(data, function (err, result) {
+			console.dir(result);
+			console.log('Done');
+		});
 	});
 }
---
+++ static/fileInput.js
@@ -0,0 +1,9 @@
+console.log("fileInput loaded");
+document.getElementById("upload-input").addEventListener('change', fileSelectionChangedHandler);
+
+function fileSelectionChangedHandler(e){
+	console.log("fileSelectionChangedHandler e:"+e.value);
+	var files = document.getElementById("upload-input").files;
+	document.getElementById("fileSelectionForm").submit();
+	e.preventDefault();
+}
